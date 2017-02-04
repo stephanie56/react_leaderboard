6729c2422e5be3af85772a7c8d073330cecfbe75
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-02-04 01:56:22 -0500

    Updated style: dark theme to the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
 
     return (
-      <div id="header">{this.props.logo}</div>
+      <div id="header"><img src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" /></div>
     )
   }
 }
