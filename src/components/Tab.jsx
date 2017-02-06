f6abd6de99ff873a577a67a508de2e8c40564595
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-02-06 00:00:32 -0500

    Fixed: handleChange function in Tab component

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default class Tab extends React.Component {
 
-  handleChange(clicked_id){
-    this.props.handleData(clicked_id);
+  handleChange(e){
+    this.props.handleData(e.target.id);
   }
 
   render() {
     return (
       <div>
-      <button id="recent" type="button" onClick={this.handleChange(this.id)}>Last 30</button>
-      <button id="alltime" type="button" onClick={this.handleChange(this.id)}>All Time</button>
+      <button id="recent" type="button" onClick={this.handleChange.bind(this)}>Last 30</button>
+      <button id="alltime" type="button" onClick={this.handleChange.bind(this)}>All Time</button>
       </div>
     )
   }
